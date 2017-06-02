da7fc4a22f4196c07d3845590c5ba2539875147b
Scukerman <nngoblin@gmail.com>
2017-06-02 13:55:07 +0300

    Total module rewrite. Use components instead of directives. ES6 syntax and other major changes. Sweepstakes mode.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var ngAnnotate = require('gulp-ng-annotate');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('default', ['compress']);
 
 gulp.task('compress', function() {
 	return gulp.src(['src/balanceChanger.js'])
+		.pipe(babel({presets: ['es2015']}))
 		.pipe(ngAnnotate())
 		.pipe(uglify())
 		.pipe(rename('balanceChanger.min.js'))
