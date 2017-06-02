55524dc0d7210639c586cd7c66e58d4f0d0bc84d
Scukerman <nngoblin@gmail.com>
2017-06-02 13:55:07 +0300

    Total module rewrite. Use components instead of directives. ES6 syntax and other major changes. Sweepstakes mode.

--- src/balanceChanger.js
+++ src/balanceChanger.js
@@ -1,118 +1,188 @@
 (function() {
-	var m = angular.module('nn.balanceChanger', []);
+	angular.module('nn.balanceChanger', ['ui.bootstrap'])
+	.run(['$templateCache', function($templateCache) {
+		$templateCache.put('balance-changer/control.html', `
+		<div class="balance-changer-control">
+			<button class="btn btn-xs btn-danger" ng-click="$ctrl.open('withdraw')" ng-if="$ctrl.model.buttons.w" title="{{ ::__(\'balanceChanger\', \'type.withdraw\') }}">-</button>
+			<button class="btn btn-xs btn-success" ng-click="$ctrl.open('deposit')" ng-if="$ctrl.model.buttons.d && !$ctrl.model.buttons.w" title="{{ ::__(\'balanceChanger\', \'type.deposit\') }}">+</button>
+			<span>{{ $ctrl.model.subject.model[$ctrl.balanceField] | nnCurrency }}</span>
+			<button class="btn btn-xs btn-success" ng-click="$ctrl.open('deposit')" ng-if="$ctrl.model.buttons.d && $ctrl.model.buttons.w" title="{{ ::__(\'balanceChanger\', \'type.deposit\') }}">+</button>
+			<button class="btn btn-xs" ng-click="$ctrl.open('ss')" ng-if="$ctrl.model.buttons.ss && $root.Auth.cashier.can_move_sweepstakes" title="{{ ::__(\'balanceChanger\', \'type.sweepstakes\') }}">SS</button>
+		</div>`);
+	}])
+	.component('balanceChangerControl', {
+		templateUrl: 'balance-changer/control.html',
+		bindings: {
+			model: '=',
+		},
+		controller: [
+			'$scope', '$uibModal', 'Auth', 'Remote', 'Alert',
+			function($scope, $uibModal, Auth, Remote, Alert) {
+				const model = this.model;
 
-	m.factory('BalanceChanger', [function() {
-		return {
-			balanceField: '',
-			method: '',
-			type: null,
-			amount: 0,
-			params: {
-				object: {
-					type: null,
-					model: {}
-				},
-				subject: {
-					type: null,
-					model: {}
-				}
-			}
-		};
-	}]);
+				// set defaults
+				_.defaults(model, {
+					method: 'Cashier.BankGroup.Transaction.moveMoney',
+					buttons: {w: 1, d: 1, ss: 0},
+					object: {type: 'bankGroup'},
+					ssDirection: 'win2bet',
+				});
 
-	m.directive('balanceChangerControl', ['BalanceChanger', function(BalanceChanger) {
-		return {
-			restrict: 'E',
-			require: 'ngModel',
-			replace: true,
-			template: '<div class="balance-changer-control"><button class="btn btn-xs btn-danger" ng-click="openModal($event, \'withdraw\')" title="{{ ::__(\'balanceChanger\', \'type.withdraw\') }}">-</button><span>{{ model[balanceField] | nnCurrency }}</span><button class="btn btn-xs btn-success" ng-click="openModal($event, \'deposit\')" title="{{ ::__(\'balanceChanger\', \'type.deposit\') }}">+</button></div>',
-			link: function($scope, $el, attrs, ngModel) {
-				$scope.model = null;
-				$scope.balanceField = attrs.field || 'balance';
-				var bc = BalanceChanger;
-
-				ngModel.$render = function() {
-					$scope.model = ngModel.$viewValue;
-				};
-
-				$scope.openModal = function($event, type) {
-					bc.method = attrs.method || 'Cashier.Player.Balance.change';
-					bc.balanceField = $scope.balanceField;
-					bc.type = type;
-
-					bc.amount = null;
-					bc.params = angular.extend({
-						amount: 0,
-						object: {
-							type: null,
-							model: {}
-						},
-						subject: {
-							type: null,
-							model: {}
-						}
-					}, $scope.$eval(attrs.params));
-					bc.params.subject.model = $scope.model;
-
-					$('#balance-changer-modal').modal('toggle');
-				};
-			}
-		};
-	}]);
-
-	m.directive('balanceChangerModal', ['BalanceChanger', 'Remote', 'Alert', function(BalanceChanger, Remote, Alert) {
-		return {
-			restrict: 'C',
-			link: function($scope, $el, attrs) {
-				var bc = $scope.balanceChanger = BalanceChanger;
-
-				$scope.change = function() {
-					if(bc.amount <= 0) {
-						Alert.Big.Simple.Error('Amount has to be positive and greater than zero.');
-						return;
-					}
-
-					var params = {
-						amount: bc.amount
-					};
-
-					var responseBalanceField = '';
-
-					if(bc.params.method === 'Cashier.BankGroup.Transaction.moveMoney') {
-						switch(bc.type) {
-							case 'deposit':
-								params.from = [bc.params.object.type, objPath(bc.params.object.model, 'id')];
-								params.to = [bc.params.subject.type, objPath(bc.params.subject.model, 'id')];
-
-								responseBalanceField = 'toBalanceAfter';
-								break;
-							case 'withdraw':
-								params.from = [bc.params.subject.type, objPath(bc.params.subject.model, 'id')];
-								params.to = [bc.params.object.type, objPath(bc.params.object.model, 'id')];
-
-								responseBalanceField = 'fromBalanceAfter';
-								break;
-							default:
-								Alert.Big.Simple.Error('Undefined transaction type.');
-						}
+				// determine balance field
+				if(model.subject.field) {
+					if(model.subject.field.basic) {
+						this.balanceField = model.subject.field.basic;
 					} else {
-						Alert.Big.Simple.Error('Undefined method.');
+						this.balanceField = model.subject.field;
+					}
+				} else {
+					this.balanceField = 'balance';
+				}
+
+				model.balanceField = this.balanceField;
+
+				this.open = function(type) {
+					// set agent if possible
+					if(Auth.agent) {
+						model.object.model = Auth.agent;
+					} else if(model.subject.type === 'playerId') {
+						model.object.model = model.subject.model.bankGroup;
+					} else if(model.subject.type === 'bankGroup') {
+						model.object.model = model.subject.model.parent;
 					}
 
-					Remote.call(bc.params.method, params).then(function(data) {
-						var balanceAfter = objPath(data, responseBalanceField);
+					// set type
+					model.type = type;
+
+					// remove amount
+					delete model.amount;
+
+					// determine component
+					let component = 'balanceChangerBasicComponent';
+					if(type === 'ss') {
+						component = 'balanceChangerSweepstakesComponent';
+						model.type = model.ssDirection;
+					}
+
+					// open modal
+					$uibModal.open({
+						component,
+						size: 'sm',
+						resolve: {
+							model,
+						},
+					});
+				};
+			}],
+	})
+	.component('balanceChangerBasicComponent', {
+		templateUrl: `${VIEW_PATH}/partials/balance-changer/modal-basic.html`,
+		bindings: {
+			close: '&',
+			dismiss: '&',
+			resolve: '=',
+		},
+		controller(Remote, Alert) {
+			_.extend(this, this.resolve);
+			const $ctrl = this;
+
+			$ctrl.ok = () => {
+				if($ctrl.model.amount <= 0) {
+					return Alert.Big.Simple.Error('Amount has to be positive and greater than zero.');
+				}
+
+				const params = {
+					amount: $ctrl.model.amount,
+				};
+
+				let responseBalanceField = null;
+
+				switch($ctrl.model.type) {
+				case 'deposit':
+					params.from = [$ctrl.model.object.type, objPath($ctrl.model.object.model, 'id')];
+					params.to = [$ctrl.model.subject.type, objPath($ctrl.model.subject.model, 'id')];
+
+					responseBalanceField = 'toBalanceAfter';
+					break;
+				case 'withdraw':
+					params.from = [$ctrl.model.subject.type, objPath($ctrl.model.subject.model, 'id')];
+					params.to = [$ctrl.model.object.type, objPath($ctrl.model.object.model, 'id')];
+
+					responseBalanceField = 'fromBalanceAfter';
+					break;
+
+				default:
+					return Alert.Big.Simple.Error('Undefined transaction type.');
+				}
+
+				const subject = $ctrl.model.subject;
+				const balanceField = $ctrl.model.balanceField;
+
+				Remote.call('Cashier.BankGroup.Transaction.moveMoney', params)
+					.then(function(data) {
+						const balanceAfter = objPath(data, responseBalanceField);
+
 						if(balanceAfter === undefined) {
 							Alert.Big.Simple.Error('Undefined balance field.');
 						} else {
-							safeApply($scope, function() {
-								bc.params.subject.model[bc.balanceField] = balanceAfter;
-							});
+							$ctrl.model.subject.model[balanceField] = balanceAfter;
 						}
 
-						$el.modal('toggle');
-					}).then(Alert.Small.Simple.Success, Alert.Big.Simple.Error);
+						$ctrl.close();
+					})
+					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error);
+			};
+
+			$ctrl.cancel = () => {
+				$ctrl.dismiss();
+			};
+		},
+	})
+	.component('balanceChangerSweepstakesComponent', {
+		templateUrl: `${VIEW_PATH}/partials/balance-changer/modal-sweepstakes.html`,
+		bindings: {
+			close: '&',
+			dismiss: '&',
+			resolve: '=',
+		},
+		controller(Remote, Alert) {
+			_.extend(this, this.resolve);
+			const $ctrl = this;
+
+			$ctrl.ok = () => {
+				if($ctrl.model.amount <= 0) {
+					return Alert.Big.Simple.Error('Amount has to be positive and greater than zero.');
+				}
+
+				const direction = $ctrl.model.type === 'bet2win' ? -1 : 1;
+				const params = {
+					amount: direction * $ctrl.model.amount,
+					playerId: objPath($ctrl.model.subject.model, 'id'),
 				};
-			}
-		};
-	}]);
-})();
+
+				const subject = $ctrl.model.subject;
+
+				Remote.call('Cashier.Player.Balance.sweepstakesWin2Bet', params)
+					.then(function(data) {
+						if(subject.field.ss) {
+							_.each(subject.field.ss, function(field) {
+								subject.model[field] = objPath(data, field);
+							});
+						} else if(subject.model.balanceBet !== undefined && subject.model.balanceWin !== undefined) {
+							subject.model.balanceBet = objPath(data, 'balanceBet');
+							subject.model.balanceWin = objPath(data, 'balanceWin');
+						} else {
+							Alert.Big.Simple.Error('Unknown field. Nothing to update.');
+						}
+
+						$ctrl.close();
+					})
+					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error);
+			};
+
+			$ctrl.cancel = () => {
+				$ctrl.dismiss();
+			};
+		},
+	});
+}());
