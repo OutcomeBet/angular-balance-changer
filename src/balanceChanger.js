366ade3ff5bfc10300881f6a38adb4db89746f5e
HIN <hin.tm2d@gmail.com>
2019-06-26 17:19:48 +0300

    feat add bonus limit

--- src/balanceChanger.js
+++ src/balanceChanger.js
@@ -22,7 +22,6 @@
 
 				// set defaults
 				_.defaults(model, {
-					amount: 0,
 					method: 'Cashier.BankGroup.Transaction.moveMoney',
 					buttons: {w: 1, d: 1, ss: 0},
 					object: {type: 'bankGroup'},
@@ -87,22 +86,12 @@
 			dismiss: '&',
 			resolve: '=',
 		},
-		controller($scope, Remote, Alert, $filter) {
+		controller(Remote, Alert) {
 			_.extend(this, this.resolve);
 			const $ctrl = this;
 
-			$ctrl.processing = true;
-
-			$ctrl.validateBonus = () => {
-				const depositMin = objPath($ctrl.model, 'bonus.deposit_min');
-				const depositMax = objPath($ctrl.model, 'bonus.deposit_max');
-				const amount = $ctrl.model.amount;
-
-				return amount >= depositMin && amount <= depositMax;
-			};
-
 			$ctrl.ok = () => {
-				if(_.isUndefined($ctrl.model.amount) || $ctrl.model.amount <= 0) {
+				if($ctrl.model.amount <= 0) {
 					return Alert.Big.Simple.Error('Amount has to be positive and greater than zero.');
 				}
 
@@ -117,8 +106,8 @@
 					params.from = [$ctrl.model.object.type, objPath($ctrl.model.object.model, 'id')];
 					params.to = [$ctrl.model.subject.type, objPath($ctrl.model.subject.model, 'id')];
 
-					if(objPath($ctrl.model, 'bonus.bonus_id') && $ctrl.validateBonus()) {
-						params.bonusId = $ctrl.model.bonus.bonus_id;
+					if(objPath($ctrl.model, 'bonus.id')) {
+						params.withDepositBonusId = $ctrl.model.bonus.id;
 					}
 
 					responseBalanceField = 'toBalanceAfter';
@@ -137,7 +126,6 @@
 				const subject = $ctrl.model.subject;
 				const balanceField = $ctrl.model.balanceField;
 
-				$ctrl.processing = true;
 				Remote.call('Cashier.BankGroup.Transaction.moveMoney', params)
 					.then(function(data) {
 						const balanceAfter = objPath(data, responseBalanceField);
@@ -150,62 +138,12 @@
 
 						$ctrl.close();
 					})
-					.then(Alert.Small.Simple.Success, (error) => {
-						const wagerCoins = this.model.subject.model.wager;
-						if(wagerCoins > 0) {
-							const wager = $filter('nnCurrency')(wagerCoins);
-							error += "<br>The player's current wager is: " + wager;
-						}
-
-						return Alert.Big.Simple.Error(error);
-					})
-					.finally(() => {
-						// for an unknown reason it has to be like this
-						$scope.$apply(() => {
-							$ctrl.processing = false;
-						});
-					});
+					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error);
 			};
 
 			$ctrl.cancel = () => {
 				$ctrl.dismiss();
 			};
-
-			$ctrl.allMoney = (flag) => {
-				if(this.model.subject.type === 'playerId') {
-					$ctrl.processing = true;
-
-					const bankGroupId = this.model.subject.model.bank_group_id;
-					const playerId = this.model.subject.model.id;
-	
-					// request available balance
-					Remote.Cashier.Player.Balance.get({bankGroupId, playerId})
-						.then((data) => {
-							$ctrl.model.subject.balance = {
-								available: data.balance,
-								activeBonus: data.active_bonus,
-								wager: data.wager,
-							};
-
-							if(flag) {
-								// fill out the amount field with balance
-								$ctrl.model.amount = data.balance;
-							} else {
-								if(angular.isNumber($ctrl.model.amount)) {
-									// reset amount field to zero
-									$ctrl.model.amount = 0;
-								}
-							}
-						})
-						.finally(() => {
-							$ctrl.processing = false;
-						});
-				} else {
-					$ctrl.processing = false;
-				}
-			};
-
-			$ctrl.allMoney(false);
 
 			// bonuses functions
 			$ctrl.bonusValue = () => {
@@ -253,10 +191,8 @@
 			_.extend(this, this.resolve);
 			const $ctrl = this;
 
-			$ctrl.processing = false;
-
 			$ctrl.ok = () => {
-				if(_.isUndefined($ctrl.model.amount) || $ctrl.model.amount <= 0) {
+				if($ctrl.model.amount <= 0) {
 					return Alert.Big.Simple.Error('Amount has to be positive and greater than zero.');
 				}
 
@@ -268,7 +204,6 @@
 
 				const subject = $ctrl.model.subject;
 
-				$ctrl.processing = true;
 				Remote.call('Cashier.Player.Balance.sweepstakesWin2Bet', params)
 					.then(function(data) {
 						if(subject.field.ss) {
@@ -284,10 +219,7 @@
 
 						$ctrl.close();
 					})
-					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error)
-					.finally(() => {
-						$ctrl.processing = false;
-					});
+					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error);
 			};
 
 			$ctrl.cancel = () => {
@@ -306,10 +238,8 @@
 			_.extend(this, this.resolve);
 			const $ctrl = this;
 
-			$ctrl.processing = false;
-
 			$ctrl.ok = () => {
-				if(_.isUndefined($ctrl.model.amount) || $ctrl.model.amount <= 0) {
+				if($ctrl.model.amount <= 0) {
 					return Alert.Big.Simple.Error('Amount has to be positive and greater than zero.');
 				}
 
@@ -317,6 +247,7 @@
 					playerId: objPath($ctrl.model.subject.model, 'id'),
 					wagerAmount: 0,
 					bonusAmount: 0,
+					bonusLimit:0,
 					changeBalance: false,
 				};
 
@@ -327,6 +258,11 @@
 				case 'bonusBalance':
 					params.bonusAmount = amount;
 					responseBalanceField = 'bonusBalanceAfter';
+					break;
+
+				case 'bonusLimit':
+					params.bonusLimit = amount;
+					responseBalanceField = 'bonusLimitAfter';
 					break;
 
 				case 'wagerCounter':
@@ -341,7 +277,6 @@
 				const subject = $ctrl.model.subject;
 				const balanceField = $ctrl.model.balanceField;
 
-				$ctrl.processing = true;
 				Remote.call('Cashier.Player.Bonus.Balance.change', params)
 					.then(function(data) {
 						const balanceAfter = objPath(data, responseBalanceField);
@@ -354,10 +289,7 @@
 
 						$ctrl.close();
 					})
-					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error)
-					.finally(() => {
-						$ctrl.processing = false;
-					});
+					.then(Alert.Small.Simple.Success, Alert.Big.Simple.Error);
 			};
 
 			$ctrl.cancel = () => {
